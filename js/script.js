4db9bc0a7cb03879426b9e3b3fd036bb280d3731
Mahdi <m.alimohammadi13802022@gmail.com>
2023-08-26 22:30:47 +0330

    add hero section animations

--- js/script.js
+++ js/script.js
@@ -24,3 +24,40 @@ window.addEventListener('scroll',()=>{
     navbar.classList.remove('active');
     logoImage.src = 'image/1.png';
 })
+
+//---------------------------------------------------------------
+
+// hero section animations
+
+window.addEventListener('load', ()=>{
+
+    const animatedButton = document.querySelector('.shop-btn');
+    const heroTitle = document.querySelector('.hero-section .details h2');
+    const heroText = document.querySelector('.hero-section .details p');
+    const heroImage = document.querySelector('.hero-section .image');
+
+    animatedButton.style.animation = 'fadeLeft 1s linear 1.2s';
+    animatedButton.addEventListener('animationend', function() {
+        animatedButton.style.opacity = 1;
+        animatedButton.style.right = 0;
+    });
+
+    heroTitle.style.animation = 'fadeTop 1s linear .2s';
+    heroTitle.addEventListener('animationend',()=>{
+        heroTitle.style.opacity = 1;
+        heroTitle.style.right = 0;
+    })
+
+    heroText.style.animation = 'fadeTop 1s linear .3s';
+    heroText.addEventListener('animationend',()=>{
+        heroText.style.opacity = .8;
+        heroText.style.right = 0;
+    })
+
+    heroImage.style.animation = 'zoomBottom 1s linear .1s';
+    heroImage.addEventListener('animationend',()=>{
+        heroImage.style.opacity = 1;
+        heroImage.style.top = 0;
+    })
+
+})
